fa8577c852e95a721cbd5fe22101358648db0d3b
Harsimar <simarkaur1295@gmail.com>
2019-07-29 17:33:22 +0530

    Sonar Code smells

--- src/main/java/com/hcl/stocktrading/controller/HistoryController.java
+++ src/main/java/com/hcl/stocktrading/controller/HistoryController.java
@@ -3,16 +3,11 @@ package com.hcl.stocktrading.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestAttribute;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.hcl.stocktrading.dto.OrderHistoryDto;
 import com.hcl.stocktrading.dto.ResponseData;
 import com.hcl.stocktrading.service.HistoryService;
-import com.hcl.stocktrading.service.HistoryServiceImpl;
 
 @RestController
 public class HistoryController {
