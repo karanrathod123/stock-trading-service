4bd4e6873d0e6571e30ba978c0e0df98c404020d
amoljadhav531 <amoljadhav531@gmail.com>
2019-07-29 17:49:15 +0530

    change

--- src/main/java/com/hcl/stocktrading/repository/OrderDetailsRepository.java
+++ src/main/java/com/hcl/stocktrading/repository/OrderDetailsRepository.java
@@ -4,10 +4,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.hcl.stocktrading.entity.OrderDetails;
-import com.hcl.stocktrading.entity.Stocks;
 
 @Repository
 public interface OrderDetailsRepository extends JpaRepository<OrderDetails, Long> {
-	public Stocks findByStockId(OrderDetails orderDetails);
 
 }
