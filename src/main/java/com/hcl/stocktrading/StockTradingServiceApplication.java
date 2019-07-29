69bfae48cff1b62d562b0e26d28e0f5da60bb59d
piyush <piyushr999@gmail.com>
2019-07-29 14:29:57 +0530

    Minor changes

--- src/main/java/com/hcl/stocktrading/StockTradingServiceApplication.java
+++ src/main/java/com/hcl/stocktrading/StockTradingServiceApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class StocktradingserviceApplication {
+public class StockTradingServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(StocktradingserviceApplication.class, args);
+		SpringApplication.run(StockTradingServiceApplication.class, args);
 	}
 
 }
