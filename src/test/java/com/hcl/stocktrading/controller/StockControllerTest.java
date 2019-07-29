1c99894a8041bc54ed6c7871a82700054362892a
piyush <piyushr999@gmail.com>
2019-07-29 17:56:35 +0530

    Merge Controller

---
+++ src/test/java/com/hcl/stocktrading/controller/StockControllerTest.java
@@ -0,0 +1,10 @@
+package com.hcl.stocktrading.controller;
+
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class StockControllerTest {
+
+	private StockController stockController;
+}
