08f7faf79c9af75dcab0b1740ce0f6427241f6c9
piyush <piyushr999@gmail.com>
2019-07-29 15:12:41 +0530

    test added

--- src/main/java/com/hcl/stocktrading/service/OrderExecutionService.java
+++ src/main/java/com/hcl/stocktrading/service/OrderExecutionService.java
@@ -1,7 +1,9 @@
 package com.hcl.stocktrading.service;
 
+import com.hcl.stocktrading.dto.ResponseData;
+
 public interface OrderExecutionService {
 	
-	
+	public ResponseData executeOrder(Long orderId);
 
 }
---
+++ src/main/java/com/hcl/stocktrading/service/OrderExecutionServiceImpl.java
@@ -0,0 +1,13 @@
+package com.hcl.stocktrading.service;
+
+import com.hcl.stocktrading.dto.ResponseData;
+
+public class OrderExecutionServiceImpl implements OrderExecutionService {
+
+	@Override
+	public ResponseData executeOrder(Long orderId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
