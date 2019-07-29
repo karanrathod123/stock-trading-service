a5de457a291954d174628f4e966e9f10c826d1c9
amoljadhav531 <amoljadhav531@gmail.com>
2019-07-29 14:27:57 +0530

    Entity chnages

--- src/main/java/com/hcl/stocktrading/entity/Stocks.java
+++ src/main/java/com/hcl/stocktrading/entity/Stocks.java
@@ -4,6 +4,9 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToOne;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import lombok.Data;
 
@@ -14,10 +17,18 @@ public class Stocks {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long stockId;
+	
 	private String stockName;
+	
 	private Double price;
-	private Integer number;
+	
+	private Long volume;
+	
 	private String stockSymbol;
+	
 	private String stockType;
 
+	@OneToOne(mappedBy = "stockId")
+	@JsonIgnore
+	private OrderDetails orderDetails;
 }
--- src/main/java/com/hcl/stocktrading/entity/User.java
+++ src/main/java/com/hcl/stocktrading/entity/User.java
@@ -4,6 +4,9 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToOne;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import lombok.Data;
 
@@ -14,7 +17,14 @@ public class User {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long userId;
+	
 	private String name;
+	
 	private String mobile;
+	
 	private String email;
+	
+	@OneToOne(mappedBy = "userId")
+	@JsonIgnore
+	private OrderDetails orderDetails;
 }
