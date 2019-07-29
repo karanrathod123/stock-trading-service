b2a51675b5e81b5e8fcbda6a9baa38aac945cf23
karanrathod123 <karanrathod2256@gmail.com>
2019-07-29 12:57:57 +0530

    initial commit

---
+++ src/main/java/com/hcl/service/StocktradingserviceApplication.java
@@ -0,0 +1,13 @@
+package com.hcl.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StocktradingserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StocktradingserviceApplication.class, args);
+	}
+
+}
