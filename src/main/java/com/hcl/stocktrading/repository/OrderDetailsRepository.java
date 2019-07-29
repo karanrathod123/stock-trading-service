066a521be5acf17e89e5f0b63bf06735a15c35aa
piyush <piyushr999@gmail.com>
2019-07-29 16:37:09 +0530

    OrderDetails Repo changes

--- src/main/java/com/hcl/stocktrading/repository/OrderDetailsRepository.java
+++ src/main/java/com/hcl/stocktrading/repository/OrderDetailsRepository.java
@@ -1,17 +1,10 @@
 package com.hcl.stocktrading.repository;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.hcl.stocktrading.dto.OrderHistoryDto;
 import com.hcl.stocktrading.entity.OrderDetails;
-import com.hcl.stocktrading.entity.User;
 
 @Repository
-public interface OrderDetailsRepository extends JpaRepository<OrderDetails, Long>{
-	
-	public List<OrderDetails> findByUserId(OrderHistoryDto userId);
-
+public interface OrderDetailsRepository extends JpaRepository<OrderDetails, Long>{	
 }
