be7d33d0f1ca1d117c0188602e1a8ebb8b9d781f
Harsimar <simarkaur1295@gmail.com>
2019-07-29 15:21:21 +0530

    ConfirmOrderService additions

--- src/main/java/com/hcl/stocktrading/service/HistoryService.java
+++ src/main/java/com/hcl/stocktrading/service/HistoryService.java
@@ -1,7 +1,5 @@
 package com.hcl.stocktrading.service;
 
-import org.springframework.stereotype.Service;
-
 
 public interface HistoryService {
 
--- src/main/java/com/hcl/stocktrading/service/HistoryServiceImpl.java
+++ src/main/java/com/hcl/stocktrading/service/HistoryServiceImpl.java
@@ -1,41 +1,41 @@
-package com.hcl.stocktrading.service;
-
-import java.util.Optional;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.stereotype.Service;
-
-import com.hcl.stocktrading.dto.OrderHistoryDto;
-import com.hcl.stocktrading.dto.ResponseData;
-import com.hcl.stocktrading.entity.User;
-import com.hcl.stocktrading.repository.OrderDetailsRepository;
-import com.hcl.stocktrading.repository.UserRepository;
-
-@Service
-public class HistoryServiceImpl {
-
-	@Autowired
-	OrderDetailsRepository orderDetailsRepository;
-	
-	UserRepository userRepository;
-	
-	public ResponseData getPastOrders(OrderHistoryDto orderHistoryDto)
-	{     
-		ResponseData response=new ResponseData();
-		Optional<User> user =userRepository.findById(orderHistoryDto.getUserId());
-		
-		if(!user.isPresent()) {
-			response.setHttpStatus(HttpStatus.NOT_FOUND);
-			response.setMessage("Wrong UserId");
-			return response;
-		}
-		else 
-			orderDetailsRepository.findByUserId();
-		return response;
-		
-		
-	}
-	
-	
-}
+//package com.hcl.stocktrading.service;
+//
+//import java.util.Optional;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.http.HttpStatus;
+//import org.springframework.stereotype.Service;
+//
+//import com.hcl.stocktrading.dto.OrderHistoryDto;
+//import com.hcl.stocktrading.dto.ResponseData;
+//import com.hcl.stocktrading.entity.User;
+//import com.hcl.stocktrading.repository.OrderDetailsRepository;
+//import com.hcl.stocktrading.repository.UserRepository;
+//
+//@Service
+//public class HistoryServiceImpl {
+//
+//	@Autowired
+//	OrderDetailsRepository orderDetailsRepository;
+//	
+//	UserRepository userRepository;
+//	
+//	public ResponseData getPastOrders(OrderHistoryDto orderHistoryDto)
+//	{     
+//		ResponseData response=new ResponseData();
+//		Optional<User> user =userRepository.findById(orderHistoryDto.getUserId());
+//		
+//		if(!user.isPresent()) {
+//			response.setHttpStatus(HttpStatus.NOT_FOUND);
+//			response.setMessage("Wrong UserId");
+//			return response;
+//		}
+//		else 
+//			orderDetailsRepository.findByUserId();
+//		return response;
+//		
+//		
+//	}
+//	
+//	
+//}
